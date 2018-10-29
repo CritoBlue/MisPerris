5af8202479dbc111be03a324b87e4bd4b8543f6f
Cristóbal Sierpe <ryu666.crito@gmail.com>
2018-10-29 04:26:46 -0300

    Vista de Servicio y Contacto actualizados para soportar formularios de Django

--- MisPerris/views.py
+++ MisPerris/views.py
@@ -1,26 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import PostForm
+from .forms import RawFormPersona, RawFormPerro
 from .models import Region, Ciudad, TipoVivienda, Raza, EstadoPerro, Persona, Perro
 
 def index(request):
 	return render(request, 'misperris/index.html')
 
-def contacto(request):
-	regions = Region.objects.all()
-	cities = Ciudad.objects.all()
-	tipoViv = TipoVivienda.objects.all()
-	return render(request, 'misperris/contacto.html', {"regions" : regions, "cities" : cities, "types" : tipoViv})
-
-
 def servicios(request):
 	estado = EstadoPerro.objects.all()
 	raza = Raza.objects.all()
 	persona = Persona.objects.all()
-	return render(request, 'misperris/servicios.html', {"estado" : estado, "raza" : raza, "persona" : persona})
+	dogform = RawFormPerro()
 
-'''
-def create_persona (request):
-	form = forms.CreatePersona()
-	return render (request, 'misperris/contacto.html', {'form' : form})
-'''
+	if request.method == 'POST':
+		dogform = RawFormPerro(request.POST)
+		if dogform.is_valid():
+			Perro.objects.create(**dogform.cleaned_data)
+			dogform = RawFormPerro()
+		else:
+			print(dogform.errors)
+
+	context = {
+		"form": dogform,
+		"estado" : estado,
+		"raza" : raza,
+		"persona" : persona
+	}
+	return render(request, 'misperris/servicios.html', context)
+
+def contacto(request):
+	regions = Region.objects.all()
+	cities = Ciudad.objects.all()
+	tipoViv = TipoVivienda.objects.all()
+	myform = RawFormPersona()
+
+	if request.method == 'POST':
+		myform = RawFormPersona(request.POST)
+		if myform.is_valid():
+			Persona.objects.create(**myform.cleaned_data)
+			myform = RawFormPersona()
+		else:
+			print(myform.errors)
+	
+	context = {
+		"form" : myform,
+		"regions" : regions,
+		"cities" : cities,
+		"types" : tipoViv
+	}
+	return render(request, 'misperris/contacto.html', context)
